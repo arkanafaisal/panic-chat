58f1d2ab62d34fe5cca12bc1e9535f30ee569441
arkanafaisal <arkana.faisal.r@gmail.com>
2026-01-08 19:44:32 +0700

    update

--- main.js
+++ main.js
@@ -1,53 +1,54 @@
-let userSelected = document.querySelector(".selected")
-let users = document.getElementsByClassName("user")
-let chatContainer = document.getElementsByClassName("chat-container")[0]
-let chatWrap = document.getElementsByClassName("chatWrap")[0]
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty("--vh", vh + "px")
+
+
+let userSelector = document.getElementById("user-select")
+let pointer = document.getElementById("user-pointer")
+let chatContainer = document.getElementById("chat-container")
 let chats = document.getElementsByClassName("chats")
-let input = document.getElementsByTagName("input")
+let chatNode = document.getElementById("chat-node")
+let input = document.getElementById("input")
 
-let lastChat = chats[chats.length - 1]
-let person = 2
-function userSelectHandler(e){
-    userSelected.classList.remove("selected")
-    e.target.classList.add("selected")
-    userSelected = document.querySelector(".selected")
-    if(e.target.classList[1] === 'person1'){person = 1}else{person = 2}
-
-    lastChat = chats[chats.length - 1]
-    if(person === 1){
-        lastChat.style.alignSelf = "flex-start"
-    } else {
-        lastChat.style.alignSelf = "flex-end"
+let side = 2
+function userSwitch(){
+    side = side === 2? 1 : 2
+    if(side === 2){
+        pointer.classList.add("self-end")
+        chatNode.classList.add("self-end")
     }
-}
+    else{
+        pointer.classList.remove("self-end")
+        chatNode.classList.remove("self-end")
+    }
 
+    openKeyboard()
+}
+function openKeyboard(){
+    input.focus()
+}
+        
 function enterHandler(input){
-    const newChat = document.createElement("div")
-    newChat.classList.add("chats")
-    newChat.innerHTML = input
-    newChat.style.height = "1 rem"
-    newChat.style.alignSelf = lastChat.style.alignSelf
-    chatContainer.insertBefore(newChat, lastChat)
-    lastChat = chats[chats.length - 1]
+    const newChat = chatNode.cloneNode()
+    newChat.innerText = input
+    chatContainer.insertBefore(newChat, chatNode)
 }
 
 document.addEventListener("keydown", e => {
-    if(e.key === "Enter" && input[0].value.trim()){
-        enterHandler(input[0].value)
-        input[0].value = ""
+    if(e.key === "Enter" && input.value.trim()){
+        enterHandler(input.value)
+        input.value = ""
+        chatNode.innerText = "..."
     }
     if(e.key === "Tab"){
         e.preventDefault()
-        if(userSelected.classList[1] === "person1"){
-            userSelected.classList.remove("selected")
-            users[1].classList.add("selected")
-            userSelected = users[1]
-            lastChat.style.alignSelf = "flex-end"
-        } else {
-            userSelected.classList.remove("selected")
-            users[0].classList.add("selected")
-            userSelected = users[0]
-            lastChat.style.alignSelf = "flex-start"
-        }
+        userSwitch()
     }
+})
+input.addEventListener("input", e =>{
+    const inputvalue = input.value
+    if(inputvalue.trim() === ""){
+        input.value = ""
+        chatNode.innerText = "..."
+    }
+    chatNode.innerText = inputvalue
 })
